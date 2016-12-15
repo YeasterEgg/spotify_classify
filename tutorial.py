d4b85e9d042ff8ac05f558048ded64d754f7b956
YeasterEgg <l.d.mattiazzi@gmail.com>
2016-12-15 18:19:49 +0100

    whatthewhat"

--- tutorial.py
+++ tutorial.py
@@ -1,4 +1,4 @@
-from sklearn import cross_validation
+from sklearn import model_selection
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
@@ -48,6 +48,9 @@ class MachineLearning:
     self.swp = False
     self.tss = 0.2
     self.prepare_df()
+    self.seed = 8
+    self.num_folds = 10
+    self.scoring = 'accuracy'
     return self
 
   def generate_first_coefficients(self, variables):
@@ -84,12 +87,36 @@ class MachineLearning:
 
 ## TUTORIAL METHODS
 
-  def validation_dataset(self):
+  def evaluate_models(self):
     array = self.df.values
-    X = array[:1,11]
-    Y = array[:0]
-    validation_size = self.tss
-    seed = 8
-    self.X_train, self.X_validation, self.Y_train, self.Y_validation = cross_validation.train_test_split(X, Y, test_size=validation_size, random_state=seed)
+    X = array[:,1:10]
+    Y = array[:,0]
+    self.X_train, self.X_validation, self.Y_train, self.Y_validation = model_selection.train_test_split(X, Y, test_size=self.tss, random_state=self.seed)
+    num_instances = len(self.X_train)
+    self.models = []
+    self.models.append(('LR', LogisticRegression()))
+    self.models.append(('LDA', LinearDiscriminantAnalysis()))
+    self.models.append(('KNN', KNeighborsClassifier()))
+    self.models.append(('CART', DecisionTreeClassifier()))
+    self.models.append(('NB', GaussianNB()))
+    self.models.append(('SVM', SVC()))
+    # evaluate each model in turn
+    self.results = []
+    self.names = []
+    for name, model in self.models:
+      kfold = model_selection.KFold(n_splits=self.num_folds, random_state=self.seed)
+      cv_results = model_selection.cross_val_score(model, self.X_train, self.Y_train, cv=kfold, scoring=self.scoring)
+      self.results.append(cv_results)
+      self.names.append(name)
+      msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+      print(msg)
+
+  def simply_the_best(self):
+    lda = LinearDiscriminantAnalysis()
+    lda.fit(self.X_train, self.Y_train)
+    self.predictions = lda.predict(self.X_validation)
+    print(accuracy_score(self.Y_validation, self.predictions))
+    print(confusion_matrix(self.Y_validation, self.predictions))
+    print(classification_report(self.Y_validation, self.predictions))
 
 test = MachineLearning().load_test()
