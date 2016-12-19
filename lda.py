6d8f754e1a9693d065ee3765d6ba0879c2488a0a
YeasterEgg <l.d.mattiazzi@gmail.com>
2016-12-19 19:01:39 +0100

    wip

--- lda.py
+++ lda.py
@@ -3,6 +3,7 @@ import db
 import MySQLdb
 import pandas as pd
 from sklearn import model_selection
+from sklearn.externals import joblib
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 
 DB_SETTINGS = db.DatabaseInterface().return_options()
@@ -12,23 +13,32 @@ def read_database(moods_tuple):
   return pd.read_sql_query("SELECT * FROM tracks WHERE mood in {} AND `training` = 1".format(moods_tuple), con = mysql, index_col = ["spotify_id"]).drop("created_at", 1).drop("training", 1).drop("id", 1)
 
 def calculate_parameters(moods = ("happy", "sad")):
-  moods_tuple = tuple(moods)
+  moods_tuple = tuple(sorted(moods))
   filename = "_".join(mood for mood in moods_tuple)
   df = read_database(moods_tuple)
   X = df.values[:,1:10]
   Y = df.values[:,0]
-  lda = LinearDiscriminantAnalysis()
+  lda = LinearDiscriminantAnalysis(n_components=4, store_covariance=True)
   lda.fit(X, Y)
   save_model(lda, filename)
+  return lda.covariance_
 
 def save_model(lda_fit, filename):
   current_path = os.getcwd()
-  filename = os.path.join(current_path, "models", "filename.pkl")
-  with open(filename, 'wb') as fo:
+  file = os.path.join(current_path, "models", "{}.pkl".format(filename))
+  with open(file, 'wb') as fo:
     joblib.dump(lda_fit, fo)
 
-def predict_playlist(moods = ("happy", "sad"))
-  predictions = lda.predict(X_validation)
-  print(accuracy_score(self.Y_validation, self.predictions))
-  print(confusion_matrix(self.Y_validation, self.predictions))
-  print(classification_report(self.Y_validation, self.predictions))
+def predict_playlist(playlist, moods = ("happy", "sad")):
+  moods_tuple = tuple(sorted(moods))
+  filename = "_".join(mood for mood in moods_tuple)
+  lda = load_model(filename)
+  df = pd
+  calssification = lda.predict(df.values[:,1:10])
+
+def load_model(filename):
+  current_path = os.getcwd()
+  file = os.path.join(current, "models", "{}.pkl".format(filename))
+  with open(file, 'rb') as fo:
+    pca = joblib.load(fo)
+  return pca
--- spotify_mood.py
+++ spotify_mood.py
@@ -5,14 +5,32 @@ import MySQLdb
 import playlist_analyzer as pa
 import authorizer as auth
 import db
+import lda
 
 app = Flask(__name__)
 db_settings = db.DatabaseInterface().return_options()
 mysql = MySQLdb.connect(user = db_settings['user'], db = db_settings['name'], host = db_settings['host'])
 
 VERSION = "v0.2"
+
 def versionate_route(route):
   return ('/' + VERSION + '/' + route)
+
+def authorize(request, object_name):
+  if not request.json:
+    return jsonify({'error': 'Missing Json'}), 400
+  if not object_name in request.json:
+    return jsonify({'error': 'Json missing necessary data.'}), 400
+  if not 'token' in request.json:
+    return jsonify({'error': 'Needs a Token!'}), 403
+  body = request.json
+  token = body['token']['token']
+  ts = body['token']['ts']
+  # authorized = auth.Authorizer(token, ts).correct()
+  authorized = True
+  if not authorized:
+    return jsonify({'error': 'Token not Valid!', 'token': token}), 403
+  return {'success': True, 'body': body[object_name]}
 
 @app.route('/version', methods=['GET'])
 def version():
@@ -34,26 +52,35 @@ def not_found(error):
 
 @app.route(versionate_route('playlist'), methods=['POST'])
 def playlist_post():
-  if not request.json:
-    return jsonify({'error': 'WRONG'}), 400
-  if not 'playlist' in request.json:
-    return jsonify({'error': 'Json not correct'}), 400
-  if not 'token' in request.json:
-    return jsonify({'error': 'Needs a Token!'}), 403
+  auth = authorize(request, "playlist")
+  if auth['success']:
+    playlist = auth['body']
+  else:
+    return auth
 
-  body = json.loads(request.json)
-  token = body['token']['token']
-  ts = body['token']['ts']
-  authorized = auth.Authorizer(token, ts).correct()
-
-  if not authorized:
-    return jsonify({'error': 'Token not Valid!', 'token': token}), 403
-
-  playlist = body['playlist']
   parsed_playlist = pa.analyze_playlist(mysql, playlist)
   result = pa.assign_playlist(parsed_playlist)
   if result:
     return jsonify({"result": "OK", "clusters": result}), 201
+  else:
+    return jsonify("NOPE"), 500
+
+@app.route(versionate_route('reload_params'), methods=['POST'])
+def reload_params_post():
+  auth = authorize(request, "moods")
+  if auth['success']:
+    moods = auth['body']
+  else:
+    return auth
+
+  mood_tuple = tuple(moods)
+  result = lda.calculate_parameters(mood_tuple)
+  if result.any():
+    matrix = {}
+    for idx, line in enumerate(result):
+      matrix[idx] = list(line)
+    print(matrix)
+    return jsonify({"result": "OK", "covariance_matrix": matrix}), 201
   else:
     return jsonify("NOPE"), 500
 
