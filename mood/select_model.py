46eba3278abc8e354a55f6f0e01f254307ad00a1
YeasterEgg <l.d.mattiazzi@gmail.com>
2017-01-06 18:13:47 +0100

    better way to check precission

--- mood/select_model.py
+++ mood/select_model.py
@@ -9,43 +9,42 @@ from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
 import pandas as pd
+import os
 
 def read_database(mysql, moods_tuple, threshold = 1):
   return pd.read_sql_query("SELECT spotify_id, mood, duration_ms, danceability, acousticness, energy, liveness, valence, instrumentalness, tempo, speechiness, loudness FROM tracks WHERE mood in {} AND count > {}".format(moods_tuple, threshold), con = mysql, index_col = ["spotify_id"])
 
 def evaluate_models(mysql, moods, test_size = 0.2, seed = 42, num_folds = 50, scoring = "accuracy"):
   moods_tuple = tuple(sorted(moods))
-  X_train, X_validation, Y_train, Y_validation = load_db(mysql, moods_tuple, test_size, seed)
-  num_instances = len(X_train)
-  results, names, models = [], [], []
-  models.append(('LR', LogisticRegression()))
-  models.append(('LDA', LinearDiscriminantAnalysis()))
-  models.append(('KNN', KNeighborsClassifier()))
-  models.append(('CART', DecisionTreeClassifier()))
-  models.append(('NB', GaussianNB()))
-  models.append(('SVM', SVC()))
-  for name, model in models:
-    kfold = model_selection.KFold(n_splits=num_folds, random_state=seed)
-    cv_results = model_selection.cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
-    results.append(cv_results)
-    names.append(name)
-    msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
-    print(msg)
+  filename = "_".join(mood for mood in moods_tuple)
+  current_path = os.getcwd()
+  file = os.path.join(current_path, "models", "{}_results.log".format(filename))
 
-def load_db(mysql, moods_tuple, test_size, seed):
   df = read_database(mysql, moods_tuple)
   array = df.values
   X = array[:,1:10]
   Y = array[:,0]
-  return model_selection.train_test_split(X, Y, test_size=test_size, random_state=seed)
-
-def simply_the_best(mysql, moods, test_size = 0.2, seed = 42):
-  moods_tuple = tuple(sorted(moods))
-  X_train, X_validation, Y_train, Y_validation = load_db(mysql, moods_tuple, test_size, seed)
-  lda = LinearDiscriminantAnalysis()
-  lda.fit(X_train, Y_train)
-  predictions = lda.predict(X_validation)
-  print(accuracy_score(Y_validation, predictions))
-  print(confusion_matrix(Y_validation, predictions))
-  print(classification_report(Y_validation, predictions))
+  X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=test_size, random_state=seed)
+  num_instances = len(X_train)
+  results, names, models = [], [], []
+  models.append(('Logistic Regression', LogisticRegression()))
+  models.append(('Linear Discrimination Analysis', LinearDiscriminantAnalysis()))
+  models.append(('K-Neighbors Classifier', KNeighborsClassifier()))
+  models.append(('Decision Tree Classifier', DecisionTreeClassifier()))
+  models.append(('Gaussian NB', GaussianNB()))
+  models.append(('SVC', SVC()))
+  for name, model in models:
+    model.fit(X_train, Y_train)
+    predictions = model.predict(X_validation)
+    with open(file, 'a') as fo:
+      fo.write(str("#"*70))
+      fo.write(str("\n"))
+      fo.write(str(name))
+      fo.write(str("\n"))
+      fo.write(str(accuracy_score(Y_validation, predictions)))
+      fo.write(str("\n"))
+      fo.write(str(confusion_matrix(Y_validation, predictions)))
+      fo.write(str("\n"))
+      fo.write(str(classification_report(Y_validation, predictions)))
+      fo.write(str("\n"))
 
