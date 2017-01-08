9143c9c9be965ad7f414e84486c5c9f88dbd6e40
YeasterEgg <l.d.mattiazzi@gmail.com>
2017-01-08 09:14:50 +0100

    back to body

--- spotify_mood.py
+++ spotify_mood.py
@@ -59,7 +59,7 @@ def not_found(error):
 def playlist_post():
   auth = authorize(request, "playlist")
   if auth['success']:
-    playlist = auth['form']
+    playlist = auth['body']
   else:
     return auth
   result = ml.playlist_analyzer.analyze_playlist(playlist, ["happy", "sad"])
