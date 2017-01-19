951ee5165af5cacc1f0731d12d54f4986092d16a
YeasterEgg <l.d.mattiazzi@gmail.com>
2017-01-19 17:14:33 +0100

    dotenv

--- db_config/authorizer.py
+++ db_config/authorizer.py
@@ -1,11 +1,7 @@
 import hashlib
 import hmac
 import os
-from dotenv import load_dotenv
 import json
-
-dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
-load_dotenv(dotenv_path)
 
 KEY = os.environ.get('MOODSECRET') or '74cebe8a8ceefff4e89635fdd8a92777bb4fd0c541d25a1a10c12f7a126b5b3f243aea19d48c40c0f5b5a8898142a28a69094567c70a2f5e371cc6ac0af90eae'
 SPECIAL_TOKEN = bytes(KEY, "utf-8")
